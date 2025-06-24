fc18dcbb626206ac80a3e78746791f74995b8e55
Audruey Gana <audrueylermagana@gmail.com>
2025-06-24 12:56:35 +0800

    Change deployment setup

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ dotenv.config();
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "./",
+  base: "/",
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
